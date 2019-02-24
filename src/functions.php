eceb486b3ac041ad665432bc75a26178883a12c9
Ari <aristath@gmail.com>
2019-02-24 12:35:51 +0200

    merge files

--- src/functions.php
+++ src/functions.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Add extra functions and hooks.
+ *
+ * @package    Kirki
+ * @copyright  Copyright (c) 2019, Ari Stathopoulos (@aristath)
+ * @license    https://opensource.org/licenses/MIT
+ * @since      4.0
+ */
+
+add_action(
+    'customize_register',
+    function() {
+        class_alias( 'Kirki\Control\Code', 'Kirki_Control_Code' );
+    }
+);
 
 add_action(
     'customize_controls_print_footer_scripts',
